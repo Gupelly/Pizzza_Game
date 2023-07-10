24bb2961c0ab7f1bf0777b4f23d14563886ade1e
Gupelly <103359810+Gupelly@users.noreply.github.com>
2023-07-10 17:07:59 +0500

    Update IngredientArray.cs

--- Scripts/IngredientArray.cs
+++ Scripts/IngredientArray.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class IngredientArray : MonoBehaviour
 {
-    // Добавить сюда все ингредиенты
-    public static Ingredient[] Ingredients;
+    public static Ingredient[] Ingredients = {new Ingredient("Pepperoni", new GameObject(), true, "red"),
+    new Ingredient("Bacon", new GameObject(), true, "red"),
+    new Ingredient("Shrimps", new GameObject(), true, "red"),
+    new Ingredient("Cucumber", new GameObject(), false, "green"),
+    new Ingredient("Tomato", new GameObject(), false, "red"),
+    new Ingredient("Olives", new GameObject(), false, "black"),
+    new Ingredient("Mushroom", new GameObject(), false, "white"),
+    new Ingredient("Onion", new GameObject(), false, "purple"),
+    new Ingredient("Greens", new GameObject(), false, "green"),};
 }
